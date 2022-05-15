7e5bfd9d4f4da0252228a8364948b534be40d123
Ronin Cunningham <ronincunningham1@gmail.com>
2022-05-14 18:06:53 -0700

    parsing some form data

--- home.js
+++ home.js
@@ -1,4 +1,24 @@
+let stringRecipes = JSON.stringify(
+	[
+		{
+			title: "pizza",
+			ingredients: ["dough", "salami", "cheese"],
+			instructions: ["Need the dough", "add the salami and cheese", "bake in oven"]
+		},
+		{
+			title: "pasta",
+			ingredients: ["dough", "tomatoes", "cheese"],
+			instructions: ["Need the dough", "add the tomatoes and cheese", "boil the pasta", "pour sauce"]
+		},
+	]
+);
+
+function deleteAllCards() {
+	// TODO !!!
+};
+
 function makeUL(array) {
+	// deleteAllCards();
     for(let i = 0; i < array.length; i++) {
         let item = document.createElement('li');
         item.appendChild(document.createTextNode(array[i].title)); // add to card from recipe array here!!!
@@ -7,29 +27,22 @@ function makeUL(array) {
 };
 
 function submitRecipe() {
-
+	const newRecipe = Array.from(document.querySelectorAll("#recipe-form input"))
+	.reduce((acc, input) => ({ ...acc, [input.id]: input.value}), {});
+	newRecipe.ingredients = newRecipe.ingredients.split(",");
+	newRecipe.instructions = newRecipe.instructions.split(",");
+	window.recipes.push(newRecipe);
+	makeUL(window.recipes);
 }
 
 function clearValues() {
-
+	// TODO!!!
 }
 
 window.onload = function () {
-	let stringRecipes = JSON.stringify(
-		[
-			{
-				title: "pizza",
-				ingredients: ["dough", "salami", "cheese"],
-				instructions: ["Need the dough", "add the salami and cheese", "bake in oven"]
-			},
-			{
-				title: "pasta",
-				ingredients: ["dough", "tomatoes", "cheese"],
-				instructions: ["Need the dough", "add the tomatoes and cheese", "boil the pasta", "pour sauce"]
-			},
-		]
-	);
-	
-	let recipes = JSON.parse(stringRecipes);
+	window.recipes = JSON.parse(stringRecipes);
     makeUL(recipes);
 };
+
+
+// TODO: left off at the bug that flashes the list and doesn't append to the list properly
